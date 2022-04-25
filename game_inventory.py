4319284a10fbac3e68b3fb7183a784b821dea8e8
MadalinaRadulescu <radulescumadalina90@gmail.com>
2022-04-25 19:25:04 +0300

    final

--- game_inventory.py
+++ game_inventory.py
@@ -2,39 +2,68 @@
 # This is the file where you must work.
 # Write code in the functions (and create new functions) so that they work
 # according to the requirements.
-
-
+arr = {"torch": 4, "sowrd":3}
+arrList = ["knif", "rope"]
 def display_inventory(inventory):
-    """Display the contents of the inventory in a simple way."""
-    pass
-
-
+    for k, v in inventory.items():
+        print(k + ": " + str(v))
+    
 def add_to_inventory(inventory, added_items):
-    """Add to the inventory dictionary a list of items from added_items."""
-    pass
+    for item in added_items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
 
+# print(add_to_inventory(arr, arrList))
 
 def remove_from_inventory(inventory, removed_items):
-    """Remove from the inventory dictionary a list of items from removed_items."""
-    pass
+    for item in removed_items:
+        if item in inventory:
+            inventory[item] -= 1
+            if inventory[item] < 1:
+                del inventory[item]
+        else:
+            return inventory
+    return inventory
+
+#print(remove_from_inventory(arr, arrList))
 
 
 def print_table(inventory, order=None):
-    """
-    Display the contents of the inventory in an ordered, well-organized table with
-    each column right-aligned.
-    """
-
-    pass
+    print(" -----------------")
+    print (" item name | count")
+    print(" -----------------")
+    if order is None:
+        for k, v in inventory.items():
+            count = v
+            print (" {:>10}| {:>5}".format(k, count))
+    elif order == "desc":
+        sort_inventory = sorted(inventory.items(), key = lambda x:x[1], reverse=True)
+        for i in sort_inventory:
+            print (" {:>10}| {:>5}".format(i[0], i[1]))
+    elif order == "asc":
+        sort_inventory = sorted(inventory.items(), key = lambda x:x[1])
+        for i in sort_inventory:
+            print (" {:>10}| {:>5}".format(i[0], i[1]))
+    print(" -----------------")
 
 
 def import_inventory(inventory, filename):
-    """Import new inventory items from a CSV file."""
+    import csv
+    file = open(filename)
+    csvreader = csv.reader(file) 
 
-    pass
+    list_itemes = []
+    for l in csvreader:
+        for item in l:
+            list_itemes.append(row)
+    add_to_inventory(inventory, list_itemes)
 
 
 def export_inventory(inventory, filename):
-    """Export the inventory into a CSV file."""
-
-    pass
+    with open(filename, 'w') as f:
+        for key, val in inventory.items():
+            item = ", ".join([key]*val)
+            f.write(item + ", ")
